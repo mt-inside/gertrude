eda79848d7830aac5f363798a8e3630e30528b86
Matt Turner <git@mt165.co.uk>
2023-08-29 20:27:58 +0100

    Respond to basic queries

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use nom::{
 use std::collections::HashMap;
 
 fn parse_many<'a, 'b>(i: &'a str, karma: &'b mut HashMap<String, i32>) -> IResult<&'a str, ()> {
+    // TODO: nom-unicode. basically need a set of either: all token-admissable chars, or all non-token (space & punctuation)
     let token_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-";
     let words = terminated(is_a(token_chars), opt(is_not(token_chars)));
     let mut upvote = opt(terminated(
@@ -39,7 +40,7 @@ fn parse_many<'a, 'b>(i: &'a str, karma: &'b mut HashMap<String, i32>) -> IResul
 }
 
 #[tokio::main]
-async fn main() -> Result<(), irc::error::Error> {
+async fn main() -> Result<(), anyhow::Error> {
     go("");
     go("no votes");
     go("--");
@@ -53,7 +54,7 @@ async fn main() -> Result<(), irc::error::Error> {
     Ok(())
 }
 
-async fn go_lurk() -> Result<(), irc::error::Error> {
+async fn go_lurk() -> Result<(), anyhow::Error> {
     let config = Config {
         nickname: Some("gertrude".to_owned()),
         server: Some("irc.z.je".to_owned()),
@@ -69,13 +70,23 @@ async fn go_lurk() -> Result<(), irc::error::Error> {
     while let Some(message) = stream.next().await.transpose()? {
         println!("{:?}", message);
         if let Command::PRIVMSG(ref recipient, ref text) = message.command {
-            if text.starts_with(client.current_nickname()) {
+            if text.starts_with(client.current_nickname())
+                || recipient.eq(client.current_nickname())
+            {
+                // TODO: more parsing, have a command syntax for her. Eg "karma foo"
+                // gonna be the thing of try one compiler, then another if it fails (alt?) - or if-let?, and
+                // within them, prefix? value? recognize?
+                // Actually test this parser before you deploy it!
+
                 let cur = format!("{:?}", karma);
-                client.send_privmsg(recipient, cur).unwrap();
+                println!("Sending response to {:?}", message.response_target());
+                client.send_privmsg(message.response_target().unwrap(), cur)?;
+            } else {
+                // TODO: error handling, but can't just ? it up because that (exceptionally) returns text, which doesn't live long enough
+                let res = parse_many(text, &mut karma);
+                println!("{:?}", res);
+                println!("{:?}", karma);
             }
-            let res = parse_many(text, &mut karma);
-            println!("{:?}", res);
-            println!("{:?}", karma);
         }
     }
 
