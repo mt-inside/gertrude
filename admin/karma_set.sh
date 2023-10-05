ceac6844a9c50b46f54d38b862c551ba19097ff8
Matt Turner <git@mt165.co.uk>
2023-10-05 10:05:19 +0100

    New paths to idl files in admin scripts

--- admin/karma_set.sh
+++ admin/karma_set.sh
@@ -3,4 +3,4 @@
 term="${1:?need a term}"
 value="${2:?need a value}"
 
-grpcurl -plaintext -import-path ./api/proto/admin/v1/ -proto karma.proto -d '{"term": "'${term}'", "value": '${value}'}' '[::1]:50051' admin.v1.KarmaService/Set
+grpcurl -plaintext -import-path ./idl/api/proto/admin/v1/ -proto karma.proto -d '{"term": "'${term}'", "value": '${value}'}' '[::1]:50051' admin.v1.KarmaService/Set
--- admin/plugins_list.sh
+++ admin/plugins_list.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-grpcurl -plaintext -import-path ./api/proto/admin/v1/ -proto plugins.proto -d '{}' '[::1]:50051' admin.v1.PluginsService/List
+grpcurl -plaintext -import-path ./idl/api/proto/admin/v1/ -proto plugins.proto -d '{}' '[::1]:50051' admin.v1.PluginsService/List
